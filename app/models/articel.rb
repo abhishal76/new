f4f1d4711f00e8b28c8ea75456dd719670680e3f
abhishal76 <asaini@taximagic.com>
2013-06-24 19:09:33 -0400

    first blogger commit

---
+++ app/models/articel.rb
@@ -0,0 +1,3 @@
+class Articel < ActiveRecord::Base
+  # attr_accessible :title, :body
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '604c339c2e40a8b865da38f27a85ee46c9b67040356a92d5773ab7de238d29a21d5aeaa3201dbacf76e5f79723cc86c6fc9815321d5a8f65afcead7fbd0f1324'
